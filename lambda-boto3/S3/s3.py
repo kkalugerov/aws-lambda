9789f327c4404d0fa3ea333228a7693b33b35d66
Kristiyan Kalugerov <kristiyankk@helecloud.com>
2019-12-01 18:39:25 +0200

    Add more lambda examples

--- lambda-boto3/S3/s3.py
+++ lambda-boto3/S3/s3.py
@@ -2,5 +2,7 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-for bucket in s3.buckets.all():
-  print(bucket)
+# for bucket in s3.buckets.all():
+bucket = s3.Object(bucket_name='tf-remote-state-bucket-2k19',key='experimental')
+print(bucket.bucket_name)
+print(bucket.key)
